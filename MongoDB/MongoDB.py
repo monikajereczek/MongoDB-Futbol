bc6f6c4bcc915e89105c905efe21f1ad4e50f1eb
M J <monikajereczek9@gmail.com>
2022-05-15 16:16:42 +0200

    Add project files.

---
+++ MongoDB/MongoDB.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*- coding: cp1250 -*-
+from pymongo import MongoClient
+import sys
+import os
+
+def clear():
+    os.system('cls')
+def polacz_z_baza():  
+    try:
+        uzytkownik = input("Podaj u¿ytkownika: ")
+        haslo= input("Podaj has³o: ")
+        loginstring = "mongodb+srv://"+uzytkownik+":"+haslo+"@cluster0.m9rdb.mongodb.net/Futbol?retryWrites=true&w=majority"
+        print("£¹cznie z baz¹...")
+        client=MongoClient(loginstring)
+        if client.server_info()['ok']==1:
+            print("Po³¹czono z baz¹")
+        return client, uzytkownik
+    except:
+        print("B³¹d ³¹czenia siê z baz¹")
+        sys.exit()
+def pozycja_druzyna(client):
+    druzyna=input("Podaj druzynê: ")
+    pozycja=input("Podaj pozycjê: ")
+    result = client["Futbol"]["Zawodnicy"].find({"Pozycja":pozycja , "Dru¿yna": druzyna})
+    print("Zawodnicy z druzyny "+druzyna+ " na pozycji "+pozycja+":")
+    for x in result:
+        print(x["Imiê"]+" "+x["Nazwisko"])
+    print()
+def pozycja_druzyna_zlicz(client):
+    druzyna=input("Podaj druzynê: ")
+    result = client["Futbol"]["Zawodnicy"].aggregate([ { '$match': { 'Dru¿yna': druzyna } }, 
+                                                       { '$group': { '_id': '$Pozycja', 'counter': 
+                                                       { '$count': {} } } } ])
+    print("Iloœæ zawodników z druzyny "+druzyna+ " na pozycjach:")
+    for x in result:
+        print(x['_id']+" - "+str(x['counter']))
+    print()
+def wygrane_mecze(client):
+    druzyna=input("Podaj dru¿ynê: ")
+    result = client["Futbol"]["Mecze"].aggregate([ { '$match': { '$or': [ { 'D_Domowa': { '$eq': druzyna } }, { 'D_Goscie': { '$eq': druzyna } } ] } }, 
+                                                      { '$project': { 'D_Domowa': 1, 'D_Goscie': 1, 'Wynik_Domowa': 1, 'Wynik_Goscie': 1, 
+                                                                     'wygrana': { '$cond': { 'if': { '$or': [ { '$and': [ { '$eq': [ '$D_Domowa', druzyna ] }, 
+                                                                                                                         { '$gte': [ '$Wynik_Domowa', '$Wynik_Goscie' ] } ] }, 
+                                                                                                             { '$and': [ { '$eq': [ '$D_Goscie', druzyna ] }, { '$gte': [ '$Wynik_Goscie', '$Wynik_Domowa' ] } ] } ] },
+                                                                                           'then': 1, 'else': 0 } } } }, 
+                                                        { '$match': { 'wygrana': { '$eq': 1 } } },
+                                                        { '$project': { 'wygrana': 0 } } ])
+    print("Mecze, która wygra³a dru¿yna "+druzyna+":")
+    for x in result:
+        print(x['D_Domowa']+" vs "+x['D_Goscie']+" wynik: " + str(x['Wynik_Domowa'])+'-'+str(x['Wynik_Goscie']))
+    print()
+def rozegrane_mecze_ligi(client):
+    liga=input("Podaj Ligê: ")
+    result = client["Futbol"]["Mecze"].aggregate([ { '$lookup': { 'from': 'Druzyny', 'localField': 'D_Domowa', 'foreignField': 'Nazwa', 'as': 'dane' } }, 
+                                                  { '$unwind': { 'path': '$dane' } }, { '$set': { 'Liga': '$dane.Liga' } }, { '$project': { 'dane': 0 } }, 
+                                                  { '$addFields': { 'Data': { '$dateToString': { 'format': '%Y-%m-%d', 'date': '$Data' } } } }, 
+                                                  { '$match': { 'Liga': liga } }, { '$sort': { 'Data': 1 } } ])
+    print("Mecze, rezegrane w lidze "+liga+":")
+    for x in result:
+        print(str(x['Data'])+"  "+x['D_Domowa']+" vs "+x['D_Goscie']+" wynik: " + str(x['Wynik_Domowa'])+'-'+str(x['Wynik_Goscie']))
+    print()
+def punktowi_zawodnicy(client):
+    data=input("Podaj datê meczu: ")
+    druzyna=input("Podaj dru¿ynê domow¹: ")
+    result = client["Futbol"]["Mecze"].aggregate([ { '$addFields': { 'Data': { '$dateToString': { 'format': '%Y-%m-%d', 'date': '$Data' } } } },
+                                                 { '$match': { 'Data': '2022-04-09', 'D_Domowa': 'Wataha Zielona Góra' } } ])
+    print("Zawodnicy i ich punkty:")
+    for x in result:
+        for y in x['Punkty_Zawodnicy_Domowa']:
+            zawodnik = client["Futbol"]["Zawodnicy"].aggregate([ { '$match': { 'Nr Zawodnika': y["Zawodnik"], 'Dru¿yna': druzyna } }, 
+                                                            { '$project': { '_id': 0, 'Liga': 0, 'Dru¿yna': 0 } } ])
+            for z in zawodnik:
+                print(y["Typ"]+" - "+z["Imiê"]+" "+z["Nazwisko"]+ " - liczba punktów: "+str(y['Liczba']))
+        for y in x['Punkty_Zawodnicy_Goscie']:
+            zawodnik = client["Futbol"]["Zawodnicy"].aggregate([ { '$match': { 'Nr Zawodnika': y["Zawodnik"], 'Dru¿yna': druzyna } }, 
+                                                            { '$project': { '_id': 0, 'Liga': 0, 'Dru¿yna': 0 } } ])
+            for z in zawodnik:
+                print(y["Typ"]+" - "+z["Imiê"]+" "+z["Nazwisko"]+ " - liczba punktów: "+str(y['Liczba']))
+    
+def menu_admin():
+    print("1.Wypisz wszystkich zawodników z dru¿yny, graj¹cych na wybranej pozycji")
+    print("2.Wypisz ilu zawodników gra na pozycji w dru¿ynie")
+    print("3.Wypisz wygrane mecze dru¿yny")
+    print("4.Wypisz rozegrane mecze danej ligii")
+    print("5.Zawodnicy, którze zdobyli punkty w meczu")
+    print("6.Dodaj dokument do dowolnej kolekcji")
+    print("7.Edutuj istj¹cy dokument")
+    print("10.Zakoñcz")
+    return int(input("Wybierz akcjê: "))        
+def menu_zawodnik():
+    print("1.Wypisz wszystkich zawodników z dru¿yny, graj¹cych na wybranej pozycji")
+    print("2.Wypisz ilu zawodników gra na pozycji w dru¿ynie")
+    print("3.Wypisz wygrane mecze dru¿yny")
+    print("4.Wypisz rozegrane mecze danej ligii")
+    print("5.Zawodnicy, którze zdobyli punkty w meczu")
+    print("10.Zakoñcz")
+    return int(input("Wybierz akcjê: "))
+client, uzytkownik=polacz_z_baza()
+
+if client.server_info()['ok']==1 and uzytkownik=="python":
+    clear()
+    while True:
+        wybor=menu_admin()
+        clear()
+        if wybor==1:
+            pozycja_druzyna(client)
+        elif wybor ==2:
+            pozycja_druzyna_zlicz(client)
+        elif wybor==3:
+            wygrane_mecze(client)
+        elif wybor==4:
+            rozegrane_mecze_ligi(client)
+        elif wybor==5:
+            punktowi_zawodnicy(client)
+        elif wybor==10:
+            break
+
+if client.server_info()['ok']==1 and uzytkownik=="zawodnik":
+    clear()
+    while True:
+        wybor=menu_zawodnik()
+        clear()
+        if wybor==1:
+            pozycja_druzyna(client)
+        elif wybor ==2:
+            pozycja_druzyna_zlicz(client)
+        elif wybor==3:
+            wygrane_mecze(client)
+        elif wybor==4:
+            rozegrane_mecze_ligi(client)
+        elif wybor==5:
+            punktowi_zawodnicy(client)
+        elif wybor==10:
+            break
+
+    
+
+
+
